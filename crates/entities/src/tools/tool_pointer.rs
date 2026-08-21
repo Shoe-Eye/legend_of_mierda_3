e09d3a54430f70645e87a8b568ae86aaf8593ba3
Sergei Surovtsev <ssurovsev@gmail.com>
2026-08-21 11:16:24 +0300

    tool pointer

---
+++ crates/entities/src/tools/tool_pointer.rs
@@ -0,0 +1,115 @@
+use bevy::prelude::*;
+use bevy_ecs_ldtk::prelude::*;
+use bevy_ecs_ldtk::{assets::LdtkProject, LdtkProjectHandle, LevelEvent, LevelIid};
+use lom_assets::sprites::CharacterAnimation;
+use lom_assets::StaticSpriteAssets;
+use lom_game::GameState;
+use lom_ldtk::ldtk::{spawn_game_world, LevelChangeEvent};
+
+use crate::player::Player;
+
+pub struct ToolPointerPlugin;
+
+impl Plugin for ToolPointerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_systems(
+            Update,
+            (init_tool_pointer_layer, draw_tool_pointer).run_if(in_state(GameState::GamePlay)),
+        );
+    }
+}
+
+#[derive(Component)]
+pub struct ToolPointerLayer {
+    pub level_iid: String,
+    pub width: u32,
+    pub height: u32,
+    pub grid_size: u32,
+}
+
+#[derive(Component)]
+pub struct ToolPointerTile {
+    pub x: u32,
+    pub y: u32,
+}
+
+pub fn init_tool_pointer_layer(
+    mut commands: Commands,
+    q_levels: Query<(Entity, &LevelIid)>,
+    mut ev_level_event: MessageReader<LevelEvent>,
+    projects: Query<&LdtkProjectHandle>,
+    project_assets: Res<Assets<LdtkProject>>,
+) {
+    for level_event in ev_level_event.read() {
+        if let LevelEvent::Spawned(spawned_level_id) = level_event {
+            for (level_entity, level_id) in q_levels.iter() {
+                if level_id.as_str() == spawned_level_id.as_str() {
+                    commands.entity(level_entity).with_children(|parent| {
+                        let project = project_assets.get(projects.single().unwrap().id()).unwrap();
+                        if let Some(ldtk_level) =
+                            project.get_raw_level_by_iid(&level_id.as_str().to_string())
+                        {
+                            if let Some(layer_instances) = ldtk_level.layer_instances.clone() {
+                                if let Some(layer) = layer_instances.iter().next() {
+                                    let width = layer.c_wid as u32;
+                                    let height = layer.c_hei as u32;
+                                    let grid_size = layer.grid_size as u32;
+
+                                    parent.spawn((
+                                        Name::new("ToolPointer"),
+                                        InheritedVisibility::default(),
+                                        Transform::from_translation(Vec3::new(8.0, 8.0, 1.0)),
+                                        ToolPointerLayer {
+                                            level_iid: level_id.as_str().to_string(),
+                                            width,
+                                            height,
+                                            grid_size,
+                                        },
+                                    ));
+                                }
+                            }
+                        }
+                    });
+                }
+            }
+        }
+    }
+}
+
+pub fn draw_tool_pointer(
+    mut commands: Commands,
+    q_player: Query<(Entity, &Transform, &CharacterAnimation, &Player)>,
+    q_tool_pointer_layer: Query<(Entity, &ToolPointerLayer)>,
+    q_tool_pointer_tiles: Query<(Entity, &ChildOf, &ToolPointerTile)>,
+) {
+    for (entity, _, _) in q_tool_pointer_tiles.iter() {
+        commands.entity(entity).despawn();
+    }
+
+    for (_, player_transform, _character_animation, _player) in q_player.iter() {
+        if let Some((tool_pointer_entity, tool_pointer_layer)) = q_tool_pointer_layer.iter().next()
+        {
+            let x =
+                f32::floor(player_transform.translation.x / (tool_pointer_layer.grid_size as f32))
+                    as u32;
+            let y =
+                f32::floor(player_transform.translation.y / (tool_pointer_layer.grid_size as f32))
+                    as u32;
+
+            commands.entity(tool_pointer_entity).with_children(
+                |parent: &mut bevy_ecs::relationship::RelatedSpawnerCommands<'_, ChildOf>| {
+                    parent.spawn((
+                        Sprite::from_color(Color::srgba(1.0, 0.0, 0.0, 0.5), Vec2::new(16.0, 16.0)),
+                        Transform::from_translation(Vec3::new(
+                            (x * tool_pointer_layer.grid_size) as f32,
+                            (y * tool_pointer_layer.grid_size) as f32,
+                            2.0,
+                        )),
+                        Name::new("ground tile"),
+                        ToolPointerTile { x, y },
+                    ));
+                },
+            );
+        }
+    }
+}
